ccefacf731846a7f406a0298a25a251291db9314
LeoG <leogorosito.lg@gmail.com>
2024-07-22 18:02:16 -0300

    viewport Meta Tag

--- src/Components/Pages/Habilidades/Habilidad.jsx
+++ src/Components/Pages/Habilidades/Habilidad.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
     const Habilidad = ({ icon, name }) => (
-        <div className="flex flex-col items-center">
-          {icon}
-           <p className="mt-2">{name}</p>
+            <div className="w-full overflow-x-hidden ">
+                <div className="flex flex-col items-center">
+            {icon}
+            <p className="mt-2">{name}</p>
+            </div>
         </div>
     );
 
